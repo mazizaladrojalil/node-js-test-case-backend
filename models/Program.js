901c8890a9175ac429aaaa7e5d1830eeeb246f4a
MuhmmdAziz <73269572+mazizaladrojalil@users.noreply.github.com>
2024-12-22 20:25:33 +0700

    Update Program.js

--- models/Program.js
+++ models/Program.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../config/database.js');
 
 const Program = sequelize.define('Program', {
   id: {
