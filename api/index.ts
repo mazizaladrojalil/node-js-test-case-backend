a7f6e9972ca20b533254d269dca9aad6d4a40e5e
MuhmmdAziz <73269572+mazizaladrojalil@users.noreply.github.com>
2024-12-23 06:55:40 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,26 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('../config/database');
+const programRoutes = require('../routes/programRoutes');
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const PORT = 5000;
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// Middleware
+app.use(bodyParser.json());
 
-module.exports = app;
+// Routes
+app.use('/api', programRoutes);
+
+// Sync database and start the server
+sequelize.sync()
+  .then(() => {
+    console.log('Database synced');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log('Failed to sync database:', error);
+  });
