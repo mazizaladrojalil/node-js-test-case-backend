fecabf40cf089611638fe4e81f1c9be1a8b095f3
MuhmmdAziz <73269572+mazizaladrojalil@users.noreply.github.com>
2024-12-23 08:46:49 +0700

    wsadsad

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
 const programRoutes = require('./routes/programRoutes');
@@ -9,6 +10,11 @@ const PORT = 5000;
 
 // Middleware
 app.use(bodyParser.json());
+
+app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 // Routes
 app.use('/api', programRoutes);
