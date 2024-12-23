22f4255686f40efd9d9233127b1f5f68d233ea84
MuhmmdAziz <73269572+mazizaladrojalil@users.noreply.github.com>
2024-12-23 08:09:14 +0700

    asdasda

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('be-test-case', 'root', null, {
-  host: 'localhost',
+const urlDB = 'mysql://root:umjZrJGfPyQnWaEOIMwhQMazHIsyPGZy@autorack.proxy.rlwy.net:50712/railway'
+
+const sequelize = new Sequelize(urlDB, {
   dialect: 'mysql',
+  logging: false,
 });
 
 module.exports = sequelize;
